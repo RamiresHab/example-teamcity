4e6dc71586220190b1d0d92a539ec3587e995f7a
Roman Khabibullin <ro.khabibullin@bi.zone>
2023-03-07 21:01:42 +0300

    fix feature/add_reply

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,6 +32,6 @@ public class WelcomerTest {
 	}
 	@Test
 	public void welcomerSaysReply(){
-		assertThat(welcomer.sayreply(), containsString("hunter"));
+		assertThat(welcomer.sayReply(), containsString("hunter"));
 	}
 }
